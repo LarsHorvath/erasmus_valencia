b5a588c9256cba73b13407e9258e748655d21700
Lars Horvath <lhorvath@student.ethz.ch>
2020-03-05 17:23:41 +0100

    added some information to InfoActivity

--- app/src/main/java/com/example/erasmusvalencia/InfosActivity.java
+++ app/src/main/java/com/example/erasmusvalencia/InfosActivity.java
@@ -3,12 +3,22 @@ package com.example.erasmusvalencia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class InfosActivity extends AppCompatActivity {
+
+    TextView chapter_transport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos);
+
+        chapter_transport = findViewById(R.id.info_transport);
+
+        //chapter_transport.setText();
+        //chapter_transport.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
