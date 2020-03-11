1be84a740fedcc0f7d7e20a1f0d4a811989118c6
Kai Ott <ottka@student.ethz.ch>
2020-03-11 10:20:20 +0100

    change name and let about activity override onbackpressed

--- app/src/main/java/com/example/erasmusvalencia/AboutActivity.java
+++ app/src/main/java/com/example/erasmusvalencia/AboutActivity.java
@@ -2,6 +2,7 @@ package com.example.erasmusvalencia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AboutActivity extends BaseThemeChangerActivity {
@@ -10,5 +11,14 @@ public class AboutActivity extends BaseThemeChangerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
     }
 }
